0279b374fd5e581df6ef421a1e5c4ef8812c6582
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-12-21 12:40:25 -0800

    Added some key bindings for kube

--- lisp/init-kubernetes.el
+++ lisp/init-kubernetes.el
@@ -1,6 +1,9 @@
 (require-package 'kubernetes)
 
 ;;; Some keybindings for basic commands.
-(global-set-key (kbd "C-x o") 'kubernetes-overview)
+(global-set-key (kbd "C-c k o") 'kubernetes-overview)
+(global-set-key (kbd "C-c k c") 'kubernetes-use-context)
+(global-set-key (kbd "C-c k n") 'kubernetes-set-namespace)
+
 
 (provide 'init-kubernetes)
