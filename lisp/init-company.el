3685c285c2fa02b39f6b3c10f99251df729fc6fd
Scott Sirowy <ssirowy@gmail.com>
2017-12-22 10:54:53 -0800

    Comment.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,4 +1,3 @@
-;; WAITING: haskell-mode sets tags-table-list globally, breaks tags-completion-at-point-function
 ;; TODO Default sort order should place [a-z] before punctuation
 
 (setq tab-always-indent 'complete)  ;; use 't when company is disabled
@@ -27,22 +26,21 @@
 ;; Suspend page-break-lines-mode while company menu is active
 ;; (see https://github.com/company-mode/company-mode/issues/416)
 (after-load 'company
-  (after-load 'page-break-lines-mode
-    (defvar sanityinc/page-break-lines-on-p nil)
-    (make-variable-buffer-local 'sanityinc/page-break-lines-on-p)
+            (after-load 'page-break-lines-mode
+                        (defvar sanityinc/page-break-lines-on-p nil)
+                        (make-variable-buffer-local 'sanityinc/page-break-lines-on-p)
 
-    (defun sanityinc/page-break-lines-disable (&rest ignore)
-      (when (setq sanityinc/page-break-lines-on-p (bound-and-true-p page-break-lines-mode))
-        (page-break-lines-mode -1)))
+                        (defun sanityinc/page-break-lines-disable (&rest ignore)
+                          (when (setq sanityinc/page-break-lines-on-p (bound-and-true-p page-break-lines-mode))
+                            (page-break-lines-mode -1)))
 
-    (defun sanityinc/page-break-lines-maybe-reenable (&rest ignore)
-      (when sanityinc/page-break-lines-on-p
-        (page-break-lines-mode 1)))
+                        (defun sanityinc/page-break-lines-maybe-reenable (&rest ignore)
+                          (when sanityinc/page-break-lines-on-p
+                            (page-break-lines-mode 1)))
 
-    (add-hook 'company-completion-started-hook 'sanityinc/page-break-lines-disable)
-    (add-hook 'company-completion-finished-hook 'sanityinc/page-break-lines-maybe-reenable)
-    (add-hook 'company-completion-cancelled-hook 'sanityinc/page-break-lines-maybe-reenable)))
-
+                        (add-hook 'company-completion-started-hook 'sanityinc/page-break-lines-disable)
+                        (add-hook 'company-completion-finished-hook 'sanityinc/page-break-lines-maybe-reenable)
+                        (add-hook 'company-completion-cancelled-hook 'sanityinc/page-break-lines-maybe-reenable)))
 
 
 (provide 'init-company)
